9cd6fd257b09161d14e827daf812983154e98eee
Austin <austin@flipsidecrypto.com>
2025-06-13 16:12:27 -0400

    traces by hash

--- models/silver/streamline/realtime/streamline__traces_by_hash_realtime.sql
+++ models/silver/streamline/realtime/streamline__traces_by_hash_realtime.sql
@@ -4,12 +4,11 @@
         func = 'streamline.udf_bulk_rest_api_v2',
         target = "{{this.schema}}.{{this.identifier}}",
         params ={ 
-            "external_table" :'traces',
+            "external_table" :'traces_by_hash',
             "sql_limit" :"30000",
             "producer_batch_size" :"30000",
             "worker_batch_size" :"10000",
-            "sql_source" :'{{this.identifier}}',
-            "exploded_key": tojson(['result']) 
+            "sql_source" :'{{this.identifier}}' 
         }
     ),
     tags = ['streamline_core_evm_realtime']
