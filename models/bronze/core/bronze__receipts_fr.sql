c862d98babee3d02a8df7732e2ee1f22a679f80e
Austin <austin@flipsidecrypto.com>
2025-09-23 11:31:01 -0400

    remove v1

--- models/bronze/core/bronze__receipts_fr.sql
+++ models/bronze/core/bronze__receipts_fr.sql
@@ -3,26 +3,38 @@
     tags = ['bronze','core','streamline_v1','phase_1']
 ) }}
 
+    WITH meta AS (
+        SELECT
+            registered_on AS _inserted_timestamp,
+            file_name,
+            CAST(SPLIT_PART(SPLIT_PART(file_name, '/', 4), '_', 1) AS INTEGER ) AS partition_key
+        FROM
+            TABLE(
+                information_schema.external_table_files(
+                    table_name => '{{ source( "bronze_streamline", "receipts_by_hash") }}'
+                )
+            ) A
+    )
 SELECT
-    partition_key,
-    block_number,
-    tx_hash,
-    VALUE,
-    DATA,
-    metadata,
-    file_name,
-    _inserted_timestamp
+    s.*,
+    b.file_name,
+    b._inserted_timestamp,
+    COALESCE(
+        s.value :"BLOCK_NUMBER" :: STRING,
+        s.value :"block_number" :: STRING,
+        s.metadata :request :"data" :id :: STRING,
+        PARSE_JSON(
+            s.metadata :request :"data"
+        ) :id :: STRING
+    ) :: INT AS block_number,
+    s.value: "TX_HASH" :: STRING AS tx_hash
 FROM
-    {{ ref('bronze__receipts_fr_v2') }}
-UNION ALL
-SELECT
-    _partition_by_block_id AS partition_key,
-    block_number,
-    tx_hash,
-    VALUE,
-    DATA,
-    metadata,
-    file_name,
-    _inserted_timestamp
-FROM
-   {{ ref('bronze__receipts_fr_v1') }}
+    {{ source( "bronze_streamline", "receipts_by_hash") }}
+    s
+    JOIN meta b
+    ON b.file_name = metadata$filename
+    AND b.partition_key = s.partition_key
+WHERE
+    b.partition_key = s.partition_key
+    AND DATA :error IS NULL
+    AND DATA IS NOT NULL
